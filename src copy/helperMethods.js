e4648d61d1086c8f1085d5eb070153dd11e13dce
Akash Arora <arora110@mail.chapman.edu>
2019-04-09 12:07:35 -0700

    Removed duplicate folder

--- src copy/helperMethods.js
+++
@@ -1,7 +0,0 @@
-// pre: Takes in a String
-// post: Returns True string is a valid phone number. False otherwise
-//       (http://zparacha.com/phone_number_javascript_regex) explains regex
-export function isValidPhoneNumber(number) {
-  const phoneNumberPattern = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
-  return phoneNumberPattern.test(number);
-}
